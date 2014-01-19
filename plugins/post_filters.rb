92175325191f0a8d91529b69b7b44a785d6bbbc9
TANABE Ken-ichi <nabeken@tknetworks.org>
2014-01-19 21:52:42 +0900

    plugins: Add a monkey patch not to ignore .xml without front-matter

--- plugins/post_filters.rb
+++ plugins/post_filters.rb
@@ -44,6 +44,31 @@ module Jekyll
         c.new(self.config)
       end
     end
+
+    def read_directories(dir = '')
+      base = File.join(self.source, dir)
+      entries = Dir.chdir(base) { filter_entries(Dir.entries('.')) }
+
+      self.read_posts(dir)
+
+      entries.each do |f|
+        f_abs = File.join(base, f)
+        f_rel = File.join(dir, f)
+        if File.directory?(f_abs)
+          next if self.dest.sub(/\/$/, '') == f_abs
+          read_directories(f_rel)
+        elsif !File.symlink?(f_abs)
+          first3 = File.open(f_abs) { |fd| fd.read(3) }
+          if first3 == "---" || f.end_with?(".xml")
+            # file appears to have a YAML header so process it as a page
+            pages << Page.new(self, self.source, dir, f)
+          else
+            # otherwise treat it as a static file
+            static_files << StaticFile.new(self, self.source, dir, f)
+          end
+        end
+      end
+    end
   end
 
   # Monkey patch for the Jekyll Post class. For the original class,
